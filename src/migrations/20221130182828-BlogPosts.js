261d78b99fd49a95c6316fdeab92fcbd02fd8dc1
Lucas <lucas_emanuel_sc@hotmail.com>
2022-11-30 16:09:16 -0300

    terminei a 1

---
+++ src/migrations/20221130182828-BlogPosts.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+  return await queryInterface.createTable('blog_posts', { 
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+      },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+   await queryInterface.dropTable('blog_posts');
+  }
+};
